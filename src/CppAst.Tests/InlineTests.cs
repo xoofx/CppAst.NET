212108d1eb679f3a9a698a85c6fd6c690a3d91cd
Alexandre Mutel <alexandre_mutel@live.com>
2019-05-25 11:42:13 +0200

    Add global variables tests, struct, enums

--- src/CppAst.Tests/InlineTests.cs
+++ src/CppAst.Tests/InlineTests.cs
@@ -1,5 +1,3 @@
-using System;
-using System.IO;
 using NUnit.Framework;
 
 namespace CppAst.Tests
@@ -82,6 +80,213 @@ namespace CppAst.Tests
                     }
                 }
                 , GetDefaultOptions().EnableMacros());
+        }
+
+        [Test]
+        public void TestGlobalVariables()
+        {
+            ParseAssert(@"
+int var0;
+int var1;
+extern int var2;
+const int var3 = 123;
+",
+                compilation =>
+                {
+                    Assert.False(compilation.HasErrors);
+
+                    Assert.AreEqual(4, compilation.Fields.Count);
+
+                    {
+                        var cppField = compilation.Fields[0];
+                        Assert.AreEqual("var0", cppField.Name);
+                        Assert.AreEqual(CppTypeKind.Primitive, cppField.Type.TypeKind);
+                        Assert.AreEqual(CppPrimitiveKind.Int, ((CppPrimitiveType)cppField.Type).Kind);
+                        Assert.AreEqual(CppVisibility.Default, cppField.Visibility);
+                        Assert.AreEqual(CppStorageQualifier.None, cppField.StorageQualifier);
+                    }
+
+                    {
+                        var cppField = compilation.Fields[1];
+                        Assert.AreEqual("var1", cppField.Name);
+                        Assert.AreEqual(CppTypeKind.Primitive, cppField.Type.TypeKind);
+                        Assert.AreEqual(CppVisibility.Default, cppField.Visibility);
+                        Assert.AreEqual(CppPrimitiveKind.Int, ((CppPrimitiveType)cppField.Type).Kind);
+                        Assert.AreEqual(CppStorageQualifier.None, cppField.StorageQualifier);
+                    }
+
+                    {
+                        var cppField = compilation.Fields[2];
+                        Assert.AreEqual("var2", cppField.Name);
+                        Assert.AreEqual(CppTypeKind.Primitive, cppField.Type.TypeKind);
+                        Assert.AreEqual(CppVisibility.Default, cppField.Visibility);
+                        Assert.AreEqual(CppPrimitiveKind.Int, ((CppPrimitiveType)cppField.Type).Kind);
+                        Assert.AreEqual(CppStorageQualifier.Extern, cppField.StorageQualifier);
+                    }
+
+                    {
+                        var cppField = compilation.Fields[3];
+                        Assert.AreEqual("var3", cppField.Name);
+                        Assert.AreEqual(CppTypeKind.Qualified, cppField.Type.TypeKind);
+                        Assert.AreEqual(CppTypeQualifier.Const, ((CppQualifiedType)cppField.Type).Qualifier);
+                        Assert.NotNull(cppField.DefaultValue);
+                        Assert.AreEqual(123, cppField.DefaultValue.Value);
+                    }
+                }
+                , GetDefaultOptions());
+        }
+
+
+        [Test]
+        public void TestStructs()
+        {
+            ParseAssert(@"
+struct Struct0
+{
+};
+
+struct Struct1 : Struct0
+{
+};
+
+struct Struct2
+{
+    int field0;
+};
+
+struct Struct3
+{
+private:
+    int field0;
+public:
+    float field1;
+};
+",
+                compilation =>
+                {
+                    Assert.False(compilation.HasErrors);
+
+                    Assert.AreEqual(4, compilation.Classes.Count);
+
+                    {
+                        var cppStruct = compilation.Classes[0];
+                        Assert.AreEqual("Struct0", cppStruct.Name);
+                        Assert.AreEqual(0, cppStruct.Fields.Count);
+                    }
+
+                    {
+                        var cppStruct = compilation.Classes[1];
+                        Assert.AreEqual("Struct1", cppStruct.Name);
+                        Assert.AreEqual(0, cppStruct.Fields.Count);
+                        Assert.AreEqual(1, cppStruct.BaseTypes.Count);
+                        Assert.True(cppStruct.BaseTypes[0].Type is CppClass);
+                        Assert.True(ReferenceEquals(compilation.Classes[0], cppStruct.BaseTypes[0].Type));
+                    }
+
+                    {
+                        var cppStruct = compilation.Classes[2];
+                        Assert.AreEqual("Struct2", cppStruct.Name);
+                        Assert.AreEqual(1, cppStruct.Fields.Count);
+                        Assert.AreEqual("field0", cppStruct.Fields[0].Name);
+                        Assert.AreEqual(CppTypeKind.Primitive, cppStruct.Fields[0].Type.TypeKind);
+                        Assert.AreEqual(CppPrimitiveKind.Int, ((CppPrimitiveType)cppStruct.Fields[0].Type).Kind);
+                    }
+
+                    {
+                        var cppStruct = compilation.Classes[3];
+                        Assert.AreEqual(2, cppStruct.Fields.Count);
+                        Assert.AreEqual("field0", cppStruct.Fields[0].Name);
+                        Assert.AreEqual(CppTypeKind.Primitive, cppStruct.Fields[0].Type.TypeKind);
+                        Assert.AreEqual(CppPrimitiveKind.Int, ((CppPrimitiveType)cppStruct.Fields[0].Type).Kind);
+                        Assert.AreEqual(CppVisibility.Private, cppStruct.Fields[0].Visibility);
+
+                        Assert.AreEqual("field1", cppStruct.Fields[1].Name);
+                        Assert.AreEqual(CppTypeKind.Primitive, cppStruct.Fields[1].Type.TypeKind);
+                        Assert.AreEqual(CppPrimitiveKind.Float, ((CppPrimitiveType)cppStruct.Fields[1].Type).Kind);
+                        Assert.AreEqual(CppVisibility.Public, cppStruct.Fields[1].Visibility);
+                    }
+                }
+                , GetDefaultOptions());
+        }
+
+
+        [Test]
+        public void TestEnums()
+        {
+            ParseAssert(@"
+enum Enum0
+{
+    Enum0_item0,
+    Enum0_item1,
+    Enum0_item2
+};
+
+enum class Enum1
+{
+    item0,
+    item1,
+    item2
+};
+
+enum class Enum2 : short
+{
+    item0 = 3,
+    item1 = 4,
+    item2 = 5
+};
+",
+                compilation =>
+                {
+                    Assert.False(compilation.HasErrors);
+
+                    Assert.AreEqual(3, compilation.Enums.Count);
+
+                    {
+                        var cppEnum = compilation.Enums[0];
+                        Assert.AreEqual("Enum0", cppEnum.Name);
+                        Assert.AreEqual(CppTypeKind.Primitive, cppEnum.IntegerType.TypeKind);
+                        Assert.AreEqual(CppPrimitiveKind.Int, ((CppPrimitiveType)cppEnum.IntegerType).Kind);
+                        Assert.AreEqual(3, cppEnum.Items.Count);
+                        Assert.False(cppEnum.IsScoped);
+                        Assert.AreEqual("Enum0_item0", cppEnum.Items[0].Name);
+                        Assert.AreEqual("Enum0_item1", cppEnum.Items[1].Name);
+                        Assert.AreEqual("Enum0_item2", cppEnum.Items[2].Name);
+                        Assert.AreEqual(0, cppEnum.Items[0].Value);
+                        Assert.AreEqual(1, cppEnum.Items[1].Value);
+                        Assert.AreEqual(2, cppEnum.Items[2].Value);
+                    }
+
+                    {
+                        var cppEnum = compilation.Enums[1];
+                        Assert.AreEqual("Enum1", cppEnum.Name);
+                        Assert.AreEqual(CppTypeKind.Primitive, cppEnum.IntegerType.TypeKind);
+                        Assert.AreEqual(CppPrimitiveKind.Int, ((CppPrimitiveType)cppEnum.IntegerType).Kind);
+                        Assert.AreEqual(3, cppEnum.Items.Count);
+                        Assert.True(cppEnum.IsScoped);
+                        Assert.AreEqual("item0", cppEnum.Items[0].Name);
+                        Assert.AreEqual("item1", cppEnum.Items[1].Name);
+                        Assert.AreEqual("item2", cppEnum.Items[2].Name);
+                        Assert.AreEqual(0, cppEnum.Items[0].Value);
+                        Assert.AreEqual(1, cppEnum.Items[1].Value);
+                        Assert.AreEqual(2, cppEnum.Items[2].Value);
+                    }
+
+                    {
+                        var cppEnum = compilation.Enums[2];
+                        Assert.AreEqual("Enum2", cppEnum.Name);
+                        Assert.AreEqual(CppTypeKind.Primitive, cppEnum.IntegerType.TypeKind);
+                        Assert.AreEqual(CppPrimitiveKind.Short, ((CppPrimitiveType)cppEnum.IntegerType).Kind);
+                        Assert.AreEqual(3, cppEnum.Items.Count);
+                        Assert.True(cppEnum.IsScoped);
+                        Assert.AreEqual("item0", cppEnum.Items[0].Name);
+                        Assert.AreEqual("item1", cppEnum.Items[1].Name);
+                        Assert.AreEqual("item2", cppEnum.Items[2].Name);
+                        Assert.AreEqual(3, cppEnum.Items[0].Value);
+                        Assert.AreEqual(4, cppEnum.Items[1].Value);
+                        Assert.AreEqual(5, cppEnum.Items[2].Value);
+                    }
+                }
+                , GetDefaultOptions());
         }
     }
 }
